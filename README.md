703b4daa84d7d5515e015adca7c536bbbfb4a51f
santiago046 <comehere665@gmail.com>
2023-11-16 14:35:07 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,63 @@
+# PPSSPP - GTA Stories Weapon Icons Pack
+
+![logo](images/logo.png)
+
+This texture pack for PPSSPP enhances the weapon icon textures in GTA: Liberty City Stories and GTA: Vice City Stories, increasing the resolution from 64x64 to 128x128.
+
+It includes two variants:
+- **Beta:** The icon colors match the beta tear gas icon color.
+- **White:** The icon colors are white, useful for creating new variants.
+
+## Installation
+
+1. Download the texture pack ZIP of your choice from the [latest release](https://github.com/santiago046/ppsspp-gta-stories-weapon-icons-pack/releases/latest) page.
+2. Open the ZIP file in PPSSPP as if it were a game.
+3. Wait for the installation process to complete.
+
+Make sure you have recently opened the chosen game before installing the pack to ensure a correct installation.
+
+If you already have a texture pack installed, adjustments will be necessary at your own risk. I am not responsible for any losses or damages.
+
+## Generation Script
+
+The `build.py` script can be used to generate ZIP packs, offering options to select the hash type, useful for adapting to other texture packs.
+
+### Usage
+
+```
+usage: build.py [-h] [-c <color>] [-r] [-t <type>] GAME
+
+Build the ZIP texture pack
+
+positional arguments:
+  GAME                  set the game. Valid values are: LCS or VCS
+
+options:
+  -h, --help            show this help message and exit
+  -c <color>, --icon-color <color>
+                        choose the icons color. Valid values are: beta,
+                        original or white (default: original)
+  -r, --reduce-hash     enable reducedHash in textures.ini file (default:
+                        False)
+  -t <type>, --hash-type <type>
+                        set the hash type for textures.ini file. Valid values
+                        are: xxh32 or xxh64 (default: xxh64)
+```
+
+### Examples
+
+1. Generate with beta color, default hash type, and disabled hash reduction for LCS:
+  ```
+  ./build.py -c beta LCS
+  ```
+
+2. Generate with default color (original), hash type xxh32, and enabled hash reduction for VCS:
+  ```
+  ./build.py -r -t xxh32 VCS
+  ```
+
+3. Generate with white color, hash type xxh64, and disabled hash reduction for LCS:
+  ```
+  ./build.py -c white -t xxh64 LCS
+  ```
+
---
+++ build.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+
+import argparse
+from pathlib import Path
+from zipfile import ZipFile
+
+PSP_GAME_CODES = {
+    "LCS": [
+        "ULES00151",
+        "ULES00182",
+        "ULKS46157",
+        "ULJM05255",
+        "ULJM05885",
+        "ULUS10041",
+    ],
+    "VCS": ["ULES00502", "ULES00503", "ULJM05884", "ULJM05297", "ULUS10160"],
+}
+
+
+def create_ini(hash_type, reduce_hash, game):
+    codes = "\n".join(
+        f"{code} = textures.ini" for code in PSP_GAME_CODES[game.upper()]
+    )
+
+    hashes_file = (
+        Path("./hashes")
+        / f"{game}_{hash_type}{'r' if reduce_hash else ''}.txt"
+    )
+
+    textures_ini = f"""[options]
+version = 1
+hash = {hash_type}
+ignoreAddress = true
+ignoreMipmap = true
+reduceHash = {'true' if reduce_hash else 'false'}
+
+[games]
+{codes}
+
+[hashes]
+{hashes_file.read_text()}
+
+[hashranges]
+
+[filtering]
+
+[reducehasranges]"""
+
+    return textures_ini
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        description="Build the ZIP texture pack",
+    )
+    parser.add_argument(
+        "-c",
+        "--icon-color",
+        choices=["beta", "original", "white"],
+        default="original",
+        help="choose the icons color. Valid values are: beta, original or white",
+        metavar="<color>",
+        type=str.lower,
+    )
+    parser.add_argument(
+        "-r",
+        "--reduce-hash",
+        action="store_true",
+        help="enable reducedHash in textures.ini file",
+    )
+    parser.add_argument(
+        "-t",
+        "--hash-type",
+        choices=["xxh32", "xxh64"],
+        default="xxh64",
+        help="set the hash type for textures.ini file. Valid values are: xxh32 or xxh64",
+        metavar="<type>",
+        type=str.lower,
+    )
+    parser.add_argument(
+        "game",
+        choices=["lcs", "vcs"],
+        help="set the game. Valid values are: LCS or VCS",
+        metavar="GAME",
+        type=str.lower,
+    )
+
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+
+    dest_zip_file = f"{args.game}-{args.hash_type}{'r' if args.reduce_hash else ''}-{args.icon_color}.zip"
+    icons_path = Path("./icons") / args.game / args.icon_color
+
+    # Pack into ZIP
+    with ZipFile(dest_zip_file, "w") as zip_file:
+        zip_file.writestr(
+            "textures.ini",
+            create_ini(args.hash_type, args.reduce_hash, args.game),
+        )
+
+        for file in icons_path.glob("*.png"):
+            zip_file.write(file, Path("HUD/weapon_icons") / file.name)
+
+
+if __name__ == "__main__":
+    main()
